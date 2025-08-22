32b2e8b9fd82e244b116cecc057387dd809c47cd
Greatteacherjubro <wisdomjh100@gmail.com>
2025-08-22 20:39:29 +0900

    feat: card-flipped develop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import "../styles/Card.css";
 
 export const Card = () => {
+  const [flipped, setFlipped] = useState(false);
+
+  const onClicked = () => {
+    setFlipped((prev) => !prev);
+  };
   return (
     <>
-      <div className="card">Card</div>
+      <div className={`card ${flipped ? "flipped" : ""}`} onClick={onClicked}>
+        <div className="card-front card-child"></div>
+        <div className="card-back card-child"></div>
+      </div>
     </>
   );
 };
