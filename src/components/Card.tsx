3c5e593c53e4b69044e2e1c122a401b2d6cce0d4
Greatteacherjubro <wisdomjh100@gmail.com>
2025-08-22 21:28:46 +0900

    feat: shuffle card system develop

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,8 +2,11 @@ import { useRef, useState } from "react";
 import "../styles/Card.css";
 
 export const Card = () => {
-  const [isAlign, setIsAlign] = useState(false);
-
+  interface myCard {
+    id: number;
+    front: string;
+    back: string;
+  }
   const cards = [
     { id: 1, front: "첫 번째 카드", back: "첫 번째 카드의 뒷면" },
     { id: 2, front: "두 번째 카드", back: "두 번째 카드의 뒷면" },
@@ -15,11 +18,31 @@ export const Card = () => {
     { id: 8, front: "세 번째 카드", back: "세 번째 카드의 뒷면" },
   ];
 
+  const [isAlign, setIsAlign] = useState(false);
+
+  const [card, setCard] = useState(cards);
+
+  const shuffleArray = (array: myCard[]) => {
+    const newArr = [...array];
+    for (let i = newArr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArr[i], newArr[j]] = [newArr[j], newArr[i]];
+    }
+    return newArr;
+  };
+
+  const onShuffle = () => {
+    const shuffled = shuffleArray(cards);
+    setCard(shuffled);
+    console.log(shuffled);
+  };
+
   const anglesRef = useRef(
     cards.map(() => Math.floor(Math.random() * 30 - 15))
   );
   const onClicked = () => {
     setIsAlign(true);
+    onShuffle();
   };
   return (
     <div className="card-wrap">
